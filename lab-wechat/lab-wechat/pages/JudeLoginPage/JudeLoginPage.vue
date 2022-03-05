a865f7a65ab5554843f5434bfbf3e52d7a86cd66
SpringZz <wang90944@gmail.com>
2022-03-05 12:33:45 +0800

    登陆时的页面跳转判断

---
+++ lab-wechat/lab-wechat/pages/JudeLoginPage/JudeLoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{},
+		onLoad() {
+			console.log('App launch');
+			// 判断标识, 使用token判断
+			let token = uni.getStorageSync('token');
+			console.log(token);
+			// 如果没有token, 则跳转到注册界面
+			if(!token){
+				uni.reLaunch({
+					url:'/pages/teacher/register'
+				})
+			}else{
+				console.log('有token');
+				// 跳转到首页，跳转tabbar页面, 必须使用这个方法
+				uni.switchTab({
+					url:'/pages/teacher/labInfo'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
